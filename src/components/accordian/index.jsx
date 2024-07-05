ca79b54c00d38aa603c4f1a88a47daa320baeb41
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-05 11:42:18 -0700

    Add React-Redux

---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,11 @@
+//single selection
+//multiple selection
+import {useSelector} from 'react-redux';
+export default function Accordian(){
+
+    const [selected, setSelected] = useSelector(null);
+
+    return <div>
+        Accordian
+    </div>
+};
