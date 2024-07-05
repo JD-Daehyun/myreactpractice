cfb52368df65ead062bb8631d63b57dc0dec4998
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-05 12:12:32 -0700

    Feature Single Selection Completed

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,11 +1,43 @@
 //single selection
 //multiple selection
-import {useSelector} from 'react-redux';
+import { useState } from 'react';
+import data from "./data";
+import "./styles.css";
+
 export default function Accordian(){
 
-    const [selected, setSelected] = useSelector(null);
+    const [selected, setSelected] = useState(null);
 
-    return <div>
-        Accordian
-    </div>
+    function handleSingleSelection(getCurrentId){
+       console.log(getCurrentId);
+       //to open and close the answer when selected the same item
+        setSelected(getCurrentId === selected ? null : getCurrentId)
+    }
+
+    return (
+    <div className='wrapper'>
+        <div className='accordian'>
+            {
+                //For real life cases where we are using API rather than dummy data, we need to verify that data exist and there is at least one dataItem
+                data && data.length > 0 ? (
+                data.map((dataItem) => (
+                <div className='item'>
+                    <div onClick ={() => handleSingleSelection(dataItem.id)} className='title'>
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                    {
+                        selected === dataItem.id ? 
+                        (<div className='content'>
+                            {dataItem.answer}
+                        </div>)
+                        : null
+                    }
+                </div>))
+               ) : (
+                 <div>No data found!</div>
+               )}
+        </div>
+        
+    </div>)
 };
