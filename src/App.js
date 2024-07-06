e2e2e187c9a28db4785c968f71769ef1786a6930
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-05 22:17:42 -0700

    Feature Load More Data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
-
+import LoadMoreData from './components/load-more-data';
 function App() {
   return (
     <div className="App">
@@ -19,6 +19,9 @@ function App() {
       {/* Image Slider Component */}
       <ImageSlider url={'https://picsum.photos/v2/list'} limit={"10"} page={"1"}/>
 
+      {/* Load More Data Component */}
+      <LoadMoreData />
+      
     </div>
   );
 }
---
+++ src/components/load-more-data/index.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react"
+import { GiNuclearWaste } from "react-icons/gi";
+import './styles.css';
+
+
+export default function LoadMoreData(){
+
+    const[loading, setLoading] = useState(false);
+    const[products, setProducts] = useState([]);
+    const[count, setCount] = useState(0);
+    const[disableButton, setDisableButton] =useState(false);
+
+    async function fetchProducts(){
+        try{
+            setLoading(true);
+            const response = await fetch(`https://dummyjson.com/products?limit=20&skip=${count ===0 ? 0 : count * 20}`);
+            const data = await response.json();
+
+            if(data && data.products && data.products.length){
+                setProducts((prevData)=>[...prevData, ...data.products]);
+                setLoading(false);
+            }
+            console.log(data);
+
+        }catch(e){
+            console.log(e);
+            setLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        fetchProducts();
+    }, [count]);
+
+    useEffect(()=>{
+        if(products && products.length === 100)
+            setDisableButton(true);
+    }, [products])
+
+    if(loading){
+        return <div>Loading Data...! Please Wait</div>
+    }
+
+    return <div className="load-more-container">
+        <div className="product-container">
+            {
+                products && products.length ?
+                products.map((product) =>
+                    <div key={product.id} className="product">
+                        <img 
+                            src={product.thumbnail}
+                            alt={product.title}
+                        />
+                        <p>{product.title}</p>
+
+                    </div>
+                )
+                : null
+            }
+        </div>
+        <div className="button-container">
+            <button disabled={disableButton}onClick={()=> setCount(count + 1)}>Load More Products</button>
+            {
+                disableButton ? <p>You have reached the bottom!</p>: null
+            }
+        </div>
+    </div>
+};
