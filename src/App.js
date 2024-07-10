17b57ba7299b254c37ead5b220521492fe308abc
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-10 13:10:01 -0700

    Fix: CSS Bug for App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
